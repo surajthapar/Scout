8230504b6e899d1ef46f3ea4535204d0ea938e27
Suraj Thapar <surajthapar.in@gmail.com>
2020-07-03 20:36:58 +0530

    index and query files

---
+++ scout/index.py
@@ -0,0 +1,5 @@
+# TODO
+# 1. Load the file to sqlite3 database
+# 2. Query DB in chunks
+# 3. Apply term functions on each document
+# 5. Create partitions (max 10k records per file?)
---
+++ scout/query.py
@@ -0,0 +1,4 @@
+# TODO
+# 1. Apply term functions on each document
+# 2. Read from partitions
+# 3. Ranking and Scoring Results

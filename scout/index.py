79dfb5f17eaa0682c13708d85419ecad7a530895
Suraj Thapar <surajthapar.in@gmail.com>
2020-07-03 20:42:31 +0530

    Load corpus function to move to db

--- scout/index.py
+++ scout/index.py
@@ -3,3 +3,13 @@
 # 2. Query DB in chunks
 # 3. Apply term functions on each document
 # 5. Create partitions (max 10k records per file?)
+
+def load_corpus(filepath: str, db: str):
+    """Load Corpus inserts data to SQLite3 database.
+
+    :param filepath: Corpus data file path.
+    :type filepath: str
+    :param db: SQLite3 database file path.
+    :type db: str
+    """
+    pass

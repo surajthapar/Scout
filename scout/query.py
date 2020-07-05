1e3888b887410d4b22a358f9393227f91eb73426
Suraj Thapar <surajthapar.in@gmail.com>
2020-07-05 17:45:47 +0530

    Adding main Scout class to handle search APIs

--- scout/query.py
+++ scout/query.py
@@ -1,4 +1,19 @@
-# TODO
-# 1. Apply term functions on each document
-# 2. Read from partitions
-# 3. Ranking and Scoring Results
+from scout import term
+
+
+class Scout:
+    # q = Scout(database="example.db")
+    # results = q.search(query="Earth is flat", k=10)
+    max_results = 5
+
+    def __init__(self, database):
+        self.database = database
+
+    def search(self, query, k=None):
+        # Convert 'query' into ngrams (term functions)
+        # Get json file paths from ngrams
+        # Read partitions and gather results
+        # Apply BM25 (F) model to each document
+        # Rank and sort the results by relevance score
+        # Return a list of book(id, summary) w/ highest relevance
+        pass

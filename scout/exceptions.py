f97223d8d406423111ec68b3116fb99baf9bc781
Suraj Thapar <surajthapar.in@gmail.com>
2020-07-05 22:45:29 +0530

    New exceptions added.

--- scout/exceptions.py
+++ scout/exceptions.py
@@ -2,3 +2,9 @@ class IndexMissingInDatabase(Exception):
     """Raised when SQLite3 fails to find index tables."""
 
 
+class UnsupportedQueryType(Exception):
+    """Raised when the search query is not a str."""
+
+
+class EmptyQuery(Exception):
+    """Raised when the search query is empty."""

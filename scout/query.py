d030925cb3ce5de15477829c4a12586d75d26c58
Suraj Thapar <surajthapar.in@gmail.com>
2020-07-05 18:00:51 +0530

    Read index meta

--- scout/query.py
+++ scout/query.py
@@ -1,3 +1,4 @@
+import sqlite3
 from scout import term
 
 
@@ -5,9 +6,26 @@ class Scout:
     # q = Scout(database="example.db")
     # results = q.search(query="Earth is flat", k=10)
     max_results = 5
+    total_documents = None
+    index_path = None
+    slices = None
 
     def __init__(self, database):
         self.database = database
+        self.read_meta()
+
+    def read_meta(self):
+        conn = sqlite3.connect(self.database)
+        c = conn.cursor()
+        c.execute(
+            "SELECT total_documents, index_path, slices FROM meta WHERE id=0;"
+        )
+        result = c.fetchall()
+        self.total_documents = result[0][0]
+        self.index_path = result[0][1]
+        self.slices = result[0][2]
+        c.close()
+        conn.close()
 
     def search(self, query, k=None):
         # Convert 'query' into ngrams (term functions)

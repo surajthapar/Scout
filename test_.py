0d4377e9a87fd20ac68b0c7ad874a502ac41d7fd
Suraj Thapar <surajthapar.in@gmail.com>
2020-07-06 02:27:19 +0530

    Fixing test class discovery

--- test_.py
+++ test_.py
@@ -2,37 +2,37 @@ import pytest
 from scout.term import tokenize, ngram, partitions
 
 
-class ScoutTerm:
-    def test_tokenizer():
+class TestScoutTerm:
+    def test_tokenizer(self):
         text = "This.. is MY way!!! or highway??"
         assert ["way", "highway"] == tokenize(text)
 
-    def test_tokenizer_err():
+    def test_tokenizer_err(self):
         with pytest.raises(TypeError):
             text = 50
             tokenize(text)
 
-    def test_empty_tokenizer():
+    def test_empty_tokenizer(self):
         text = ""
         assert [] == tokenize(text)
 
-    def test_ngram():
+    def test_ngram(self):
         words = ["hello", "britishmen"]
         ngs = ["hel", "hell", "hello", "bri", "brit",
                "briti", "britis", "british",
                "britishm", "ritishme", "itishmen"]
         assert ngs == list(ngram(words, 3, 8))
 
-    def test_ngram_err():
+    def test_ngram_err(self):
         with pytest.raises(TypeError):
             text = "what"
             ngram(text)
 
-    def test_empty_ngram():
+    def test_empty_ngram(self):
         words = []
         assert [] == list(ngram(words))
 
-    def test_partitions():
+    def test_partitions(self):
         ngrams = {"cha": [10, 20], "chan": [20]}
         parts = partitions(ngrams, "test", [1, 2])
         assert ("chan", "test/c/ch_index.json") == list(parts)[1]
